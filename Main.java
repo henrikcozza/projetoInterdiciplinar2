3573bf039a22c0e7b077df3de88ea591da6df27d
henrique alves conzatti <henrikcozza@hotmail.com>
2016-04-15 15:25:12 -0300

    Added files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package App;
+
+import gui.*;
+
+public class Main {
+
+	
+	
+	public static void main(String [] args){
+		
+		//seta menu principal (tecnica usada para ter acesso ao objeto menu sem depender da classe Main).
+		Configuration.menu = new MenuPrincipal();
+		
+	}
+}
